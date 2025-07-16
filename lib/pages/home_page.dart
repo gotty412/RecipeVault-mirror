1d30d63b0a859a6df60ae6d2dae9d98e7f07e996
Yuta Goto <yuta.b.goto@gmail.com>
2025-07-17 00:05:57 +0900

    heme_page.dartのimport 'package:flutter/foundation.dart' show kIsWeb;をコメントアウト

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
+// import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
